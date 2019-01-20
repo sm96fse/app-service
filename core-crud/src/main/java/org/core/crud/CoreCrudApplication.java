81722825d2567ac387a2d8090b4a2c1a5407e217
sagar mahapatra <sagarmahapatra96@gmail.com>
2019-01-20 14:56:00 +0530

    core crud added

---
+++ core-crud/src/main/java/org/core/crud/CoreCrudApplication.java
@@ -0,0 +1,14 @@
+package org.core.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreCrudApplication.class, args);
+	}
+
+}
+
