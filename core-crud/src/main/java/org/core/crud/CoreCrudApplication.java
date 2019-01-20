7226d7d313193d13fd34984af00fb159f7bf3c4e
THE $ KING - PC <sagarmahapatra96@gmail.com>
2019-01-20 15:03:16 +0530

    Added comment

--- core-crud/src/main/java/org/core/crud/CoreCrudApplication.java
+++ core-crud/src/main/java/org/core/crud/CoreCrudApplication.java
@@ -2,7 +2,7 @@ package org.core.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Core App
 @SpringBootApplication
 public class CoreCrudApplication {
 
