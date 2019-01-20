a87f707f1b2d9d5d8dbef9416d9854440d0d4be2
THE $ KING - PC <sagarmahapatra96@gmail.com>
2019-01-20 15:11:20 +0530

    Removed comments

--- core-crud/src/main/java/org/core/crud/CoreCrudApplication.java
+++ core-crud/src/main/java/org/core/crud/CoreCrudApplication.java
@@ -2,13 +2,10 @@ package org.core.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Core App
+
 @SpringBootApplication
 public class CoreCrudApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CoreCrudApplication.class, args);
 	}
-
 }
-
